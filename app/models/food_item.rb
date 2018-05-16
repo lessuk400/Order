9bc9a8fb380de0ae3fa6bce3c4686ac1a501b266
Alex Ost <oostashchenko@leobit.co>
2018-05-16 15:54:32 +0300

    added some interface

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,4 +1,22 @@
 class FoodItem < ApplicationRecord
   has_many :menus
   has_many :menus, through: :meals
+
+  def self.all_meals
+    types = []
+    FoodItem.all.each do |item|
+      types << item.mealType
+    end
+    return types.uniq
+  end
+
+  def self.generate_hash
+    result = {}
+    FoodItem.all.each do |item|
+      result[item.name] = item.price
+    end
+    return result
+  end
+
+
 end
